d552d1a978fa3cd348ef66b6aa30f5a7cb3dfc03
paunadal14-web <paunadal14@alumnes.ilerna.com>
2026-02-27 19:48:10 +0100

    Clase Item creada

---
+++ Joc_Pingu_Oreo/src/MODELO/Item.java
@@ -0,0 +1,35 @@
+package MODELO;
+
+public class Item {
+	
+	protected String nombre;
+	protected int cantidad;
+	
+	// CONSTRUCTOR
+	
+	public Item(String nombre, int cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	// GETTER Y SETTERS
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+	
+}
