7e0d6e5bf97372cfc98fa4f2e85ffe1d941eaf84
monirodrimarti-afk <monirodrimarti@alumnes.ilerna.com>
2026-03-02 16:13:31 +0100

    constructor setters getters i funcion no acabada de casilla

--- Joc_Pingu_Oreo/src/MODELO/Casilla.java
+++ Joc_Pingu_Oreo/src/MODELO/Casilla.java
@@ -1,10 +1,31 @@
 package MODELO;
 
 public abstract class Casilla {
-	// ATRIBUTS
+	
+	/* ATRIBUTS */
 	
 	private int posicion;
 	
-	// CONSTRUCTOR
+	/* CONSTRUCTOR */
+	public Casilla(int posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	/* GETTERS I SETTERS HEREDADOS */
+
+	public int getposicion() {
+		return this.posicion;
+	}
+
+	public void setposicion(int posicion) {
+		this.posicion = posicion;
+	}
+	
+	/*
+	abstract void realizarAccion(Partida partida, Jugador jugador) {
+		
+	}
+	*/
 	
 }
