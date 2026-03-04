c856b242bad74b545792f0b7a9f71042ed249d09
monirodrimarti-afk <monirodrimarti@alumnes.ilerna.com>
2026-03-04 17:13:31 +0100

    update inventario, arraylist, getters y setters

--- Joc_Pingu_Oreo/src/MODELO/Inventario.java
+++ Joc_Pingu_Oreo/src/MODELO/Inventario.java
@@ -1,5 +1,24 @@
 package MODELO;
 
-public class Inventario {
+import java.util.ArrayList;
 
+public class Inventario {
+	
+	/* ATRIBUTOS */
+    private ArrayList<Item> lista;
+
+    // Constructor
+    public Inventario() {
+    	this.lista = new ArrayList<>();
+    }
+    
+    // Getter
+    public ArrayList<Item> getlista() {
+    	return lista;
+    }
+    
+    // Setter
+    public void setLista(ArrayList<Item> lista) {
+        this.lista = lista;
+    }
 }
