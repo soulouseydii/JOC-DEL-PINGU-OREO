62c69a5b0adedd30f305c57dda4e2af1ef168ce4
monirodrimarti-afk <monirodrimarti@alumnes.ilerna.com>
2026-03-05 17:02:47 +0100

    update classe oso y extend con casilla con su metodo

--- Joc_Pingu_Oreo/src/MODELO/Oso.java
+++ Joc_Pingu_Oreo/src/MODELO/Oso.java
@@ -1,5 +1,19 @@
 package MODELO;
 
-public class Oso {
-
+public class Oso extends Casilla {
+	
+	/* CONSTRUCTOR */
+	
+	public Oso (int posicion) {
+		super(posicion);
+	}
+	
+    /* METODOS REALIZAR ACCION */
+	
+	@Override
+	public void realizarAccion(Partida partida, Jugador jugador) {
+        
+        // Logica de la casilla oso
+        jugador.setPosicion(0);
+	}
 }
