45a77d19b975838e4ddf64f73cf343e303f97537
soulouseydii <soulouseydii@alumnes.ilerna.com>
2026-03-05 16:24:04 +0100

    Creacio d'atributs,constructor, getters i setters de Pingui

--- Joc_Pingu_Oreo/src/MODELO/Pinguino.java
+++ Joc_Pingu_Oreo/src/MODELO/Pinguino.java
@@ -3,4 +3,58 @@ package MODELO;
 public class Pinguino extends Jugador {
 	
 
+	/* ATRIBUTO */
+	private Inventario inv;
+	
+	
+	/* CONSTRUCTOR QUE HEREDA DE JUGADOR */
+	public Pinguino(String nombre, String color, Inventario inv) {
+		super(nombre, color);
+		this.inv = inv;
+	}
+
+
+	/* GETTERS I SETTERS DEL INVENTARIO */
+	public Inventario getInv() {
+		return inv;
+	}
+
+
+	public void setInv(Inventario inv) {
+		this.inv = inv;
+	}
+	
+	
+	/* MÉTODO USAR ITEM */
+	
+	public void usarItem (Item i) {
+		
+		
+	}
+	
+	/* MÉTODO AÑADIR ITEM */
+	
+	public void añadirItem (Item i) {
+		
+		
+	}
+	
+	
+	/* MÉTODO QUITAR ITEM */
+	
+	public void quitarItem (Item i) {
+		
+		
+	}
+	
+	/* MÉTODO GESTIONAR BATALLA */
+	
+	public void gestionarBatalla (Pinguino p) {
+		
+		
+		
+	}
+	
+	
+	
 }
