87e0cf02a41f88c63d02b28c83472a0a19ffb337
paunadal14-web <paunadal14@alumnes.ilerna.com>
2026-03-05 15:57:17 +0100

    Cambios en Foca
    - boolean
    - constructor
    - getters i setters

--- Joc_Pingu_Oreo/src/MODELO/Foca.java
+++ Joc_Pingu_Oreo/src/MODELO/Foca.java
@@ -2,4 +2,21 @@ package MODELO;
 
 public class Foca extends Jugador {
 
+	private boolean soborno;
+	
+	public Foca(String nombre, String color) {
+		super(nombre, color);
+		this.setSoborno(false);
+	}
+	
+	//GETTERS Y SETTERS
+	
+	public boolean isSoborno() {
+		return soborno;
+	}
+
+	public void setSoborno(boolean soborno) {
+		this.soborno = soborno;
+	}
+	
 }
