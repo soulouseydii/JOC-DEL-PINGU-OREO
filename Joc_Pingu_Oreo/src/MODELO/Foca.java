979cb13603274890dfb03de2aeb829ebd160d6bd
monirodrimarti-afk <monirodrimarti@alumnes.ilerna.com>
2026-03-12 17:06:12 +0100

    update foca y atributos y cambios a gestorpartida
    cambiamos el atributo de foca para que podamos controlar los turnos que este bloqueada, pasa de boolean a int y adaptamos la funcion que habia en gestorpartida

--- Joc_Pingu_Oreo/src/MODELO/Foca.java
+++ Joc_Pingu_Oreo/src/MODELO/Foca.java
@@ -2,48 +2,53 @@ package MODELO;
 
 public class Foca extends Jugador {
 
-	private boolean soborno;
+    // Cambiamos el boolean por un int para contar los turnos
+    private int turnosBloqueada; 
 
-	// CONSTRUCTOR
-	public Foca(String nombre, String color, int posicion) {
-		super(nombre, color, posicion);
-		this.soborno = false;
-	}
-	
-	
-	//GETTERS Y SETTERS
-	
-	public boolean isSoborno() {
-		return soborno;
-	}
-
-	public void setSoborno(boolean soborno) {
-		this.soborno = soborno;
-	}
-	
-	public void aplastarJugador(Pinguino p) {
-		int cantidadActual = p.getInv().getlista().size();
-		int aQuitar = cantidadActual / 2;
-		
-		System.out.println("¡La foca aplasta a " + p.getNombre() + " al pasar por su casilla!");
-		
-		for (int i = 0; i < aQuitar; i++) {
-			if (!p.getInv().getlista().isEmpty()) {
-				p.getInv().getlista().remove(0);
-			}
-		}
-		
-		System.out.println(p.getNombre() + " ha perdido " + aQuitar + " objetos de su inventario.");
-		
-	}
-	
-	public void golpearJugador(Pinguino p) {
-		System.out.println("¡La foca golpea a " + p.getNombre() + " con la cola!");
-	}
-	
-	public boolean esSobornado() {
-        return this.soborno;
+    // CONSTRUCTOR
+    public Foca(String nombre, String color, int posicion) {
+        super(nombre, color, posicion);
+        this.turnosBloqueada = 0; // Al principio no esta bloqueada
     }
-	
-	
+    
+    // GETTERS Y SETTERS
+    public int getTurnosBloqueada() {
+        return turnosBloqueada;
+    }
+
+    public void setTurnosBloqueada(int turnos) {
+        this.turnosBloqueada = turnos;
+    }
+
+    // Metodo para que el GestorPartida le reste turnos cada vez que le toque
+    public void reducirBloqueo() {
+        if (this.turnosBloqueada > 0) {
+            this.turnosBloqueada--;
+        }
+    }
+
+    // Logica aplastar jugador
+    public void aplastarJugador(Pinguino p) {
+        int cantidadActual = p.getInv().getlista().size();
+        int aQuitar = cantidadActual / 2;
+        
+        System.out.println("¡La foca aplasta a " + p.getNombre() + " al pasar por su casilla!");
+        
+        for (int i = 0; i < aQuitar; i++) {
+            if (!p.getInv().getlista().isEmpty()) {
+                p.getInv().getlista().remove(0);
+            }
+        }
+        System.out.println(p.getNombre() + " ha perdido " + aQuitar + " objetos.");
+    }
+    
+    // Logica de golpearjugador
+    public void golpearJugador(Pinguino p) {
+        System.out.println("¡La foca golpea a " + p.getNombre() + " con la cola!");
+    }
+    
+    // Este metodo sustituye a esSobornado para que el controlador pregunte si puede mover
+    public boolean estaBloqueada() {
+        return this.turnosBloqueada > 0;
+    }
 }
