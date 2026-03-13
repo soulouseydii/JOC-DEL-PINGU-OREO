639caf9c5f0deaaf35bbbc162e93ef49ae1fed24
soulouseydii <soulouseydii@alumnes.ilerna.com>
2026-03-13 16:36:16 +0100

    Finalitzacio del metodo ejecutarCasilla, deL GestorTablero.

--- Joc_Pingu_Oreo/src/CONTROLADOR/GestorTablero.java
+++ Joc_Pingu_Oreo/src/CONTROLADOR/GestorTablero.java
@@ -6,6 +6,22 @@ public class GestorTablero {
 	
 	/* Metodo buscarAgujeroAnterior para GestorPartida y foca*/
 	
+	// MÈTODO EJECUTARCASILLA 
+	
+	public void ejecutarCasilla(Partida partida, Pinguino p, Casilla c) {
+		
+		// validamos si la casilla o el pinguino es nulo
+		
+		if (c == null || p == null) {
+			return;
+		}
+		
+		// Miramos en la casilla que cae el jugador, y ejecutamos la accion de la casilla.
+		System.out.println("El jugador cayó en: " + c.getClass().getSimpleName());
+		c.realizarAccion(partida, p);
+		
+	}
+	
 	
 	
 }
